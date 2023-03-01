b81e7f0acd829a6416464dc184ad290c2f88c9fb
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-03-01 12:21:35 +0200

    add update and delete methods

--- dist/controllers/todo.js
+++ dist/controllers/todo.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTodos = exports.createTodo = void 0;
+exports.removeTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 // RequestHandler is shortcut for {Request, Response, NextFunction}
 const todo_1 = require("../models/todo");
 const TODOS = [];
@@ -16,3 +16,23 @@ const getTodos = (req, res, next) => {
     res.json({ todos: TODOS });
 };
 exports.getTodos = getTodos;
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0)
+        throw new Error(`Could not find an item with id ${todoId}`);
+    TODOS[todoIndex].text = updatedText;
+    // TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+    res.json({ message: "Updated", updatedTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
+const removeTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0)
+        throw new Error(`Could not find an item with id ${todoId}`);
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: "Todo deleted" });
+};
+exports.removeTodo = removeTodo;
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createTodo, getTodos } from "../controllers/todo";
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  removeTodo,
+} from "../controllers/todo";
 
 const router = Router();
 
 router.get("/", getTodos);
 router.post("/", createTodo);
-router.patch("/:id");
-router.delete("/:id");
+router.patch("/:id", updateTodo);
+router.delete("/:id", removeTodo);
 
 export default router;
